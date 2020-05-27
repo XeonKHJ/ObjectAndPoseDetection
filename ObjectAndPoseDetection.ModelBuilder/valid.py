93146e8c7f54f46f078832ff8cb73c1a706b81c5
康宏嘉 <redalertkhj@live.cn>
2020-05-27 11:58:59 +0800

    完成召回率精确率的测试

--- ObjectAndPoseDetection.ModelBuilder/valid.py
+++ ObjectAndPoseDetection.ModelBuilder/valid.py
@@ -278,5 +278,5 @@ if __name__ == '__main__':
     modelcfg   = args.modelcfg
     weightfile = args.weightfile
 
-    
-    valid(datacfg, modelcfg, weightfile, 0.85)
+    for i in [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.95, 1]:
+        valid(datacfg, modelcfg, weightfile, i)
