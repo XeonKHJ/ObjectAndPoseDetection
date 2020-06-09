f5b83512355c89907de55be4ab81a74810728d2e
康宏嘉 <redalertkhj@live.cn>
2020-06-09 16:17:25 +0800

    完成输出多物体的真实边框。

--- ObjectAndPoseDetection.ModelBuilder/multi_obj_pose_estimation/PredictAndDrawBoxes.py
+++ ObjectAndPoseDetection.ModelBuilder/multi_obj_pose_estimation/PredictAndDrawBoxes.py
@@ -40,34 +40,46 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
     height = img.shape[0]
 
     box = boxes
+    x1 = int(round((box[0] - box[2]/2.0) * width))
+    y1 = int(round((box[1] - box[3]/2.0) * height))
+    x2 = int(round((box[0] + box[2]/2.0) * width))
+    y2 = int(round((box[1] + box[3]/2.0) * height))
  
-
+    points = list()
+    for p in range(1, 9):
+        points.append((box[2*p], box[2*p + 1]))
 
     if color:
         rgb = color
     else:
         rgb = (255, 0, 0)
+    if len(box) >= 7 and class_names:
+        cls_conf = box[18]
+        cls_id = box[19]
+        #print('%s: %f' % (class_names[cls_id], cls_conf))
+        classes = len(class_names)
+        offset = cls_id * 123457 % classes
+        red   = get_color(2, offset, classes)
+        green = get_color(1, offset, classes)
+        blue  = get_color(0, offset, classes)
+        if color is None:
+            rgb = (red, green, blue)
+        #img = cv2.putText(img, class_names[cls_id], (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
 
     thickness = 2
     #画线
-
-    for box in boxes:
-        points = list()
-        for p in range(1, 9):
-            points.append((box[2*p], box[2*p + 1]))
-
-        img = cv2.line(img, points[0], points[1], rgb, thickness)
-        img = cv2.line(img, points[0], points[4], rgb, thickness)
-        img = cv2.line(img, points[1], points[5], rgb, thickness)
-        img = cv2.line(img, points[4], points[5], rgb, thickness)
-        img = cv2.line(img, points[5], points[7], rgb, thickness)
-        img = cv2.line(img, points[1], points[3], rgb, thickness)
-        img = cv2.line(img, points[4], points[6], rgb, thickness)
-        img = cv2.line(img, points[0], points[2], rgb, thickness)
-        img = cv2.line(img, points[2], points[6], rgb, thickness)
-        img = cv2.line(img, points[2], points[3], rgb, thickness)
-        img = cv2.line(img, points[3], points[7], rgb, thickness)
-        img = cv2.line(img, points[7], points[6], rgb, thickness)
+    img = cv2.line(img, points[0], points[1], rgb, thickness)
+    img = cv2.line(img, points[0], points[4], rgb, thickness)
+    img = cv2.line(img, points[1], points[5], rgb, thickness)
+    img = cv2.line(img, points[4], points[5], rgb, thickness)
+    img = cv2.line(img, points[5], points[7], rgb, thickness)
+    img = cv2.line(img, points[1], points[3], rgb, thickness)
+    img = cv2.line(img, points[4], points[6], rgb, thickness)
+    img = cv2.line(img, points[0], points[2], rgb, thickness)
+    img = cv2.line(img, points[2], points[6], rgb, thickness)
+    img = cv2.line(img, points[2], points[3], rgb, thickness)
+    img = cv2.line(img, points[3], points[7], rgb, thickness)
+    img = cv2.line(img, points[7], points[6], rgb, thickness)
     #img = cv2.rectangle(img, (x1,y1), (x2,y2), rgb, 1)
     if savename:
         print("save plot results to %s" % savename)
@@ -98,24 +110,45 @@ if __name__ == '__main__':
     test_width = 416
     test_height = 416
 
-    datasetPath = '../Assets/singleshotpose/LINEMOD/'
+    datasetPath = '../Assets/DataSets/LINEMOD/'
     datasetImagePaths = datasetPath + 'benchvise/JPEGImages/'
+    outputPath = '../Assets/Outputs/Multi/'
 
     labelFolders = list()
 
-    for className in ['ape', 'cam', 'can', 'cat', 'driller', 'duck', 'eggbox', 'glue', 'holepuncher', 'iron', 'lamp']:
-        path = datasetPath + className + '/labels_occlusion/'
-        labelFolders.append(className)
+    for className in ['ape', 'benchvise', 'can', 'cat', 'duck', 'driller', 'glue']:
+        if className == 'benchvise':
+            path = datasetPath + className + '/labels/'
+        else:
+            path = datasetPath + className + '/labels_occlusion/'
+        labelFolders.append(path)
 
     imageFiles = os.listdir(datasetImagePaths)
 
     #所有图片的路径
     imagePaths = list()
-
-    for file in imageFilePaths:
-        if os.path.splitext(file)[-1] == ".jpg":
-            imagePaths.append(inputFolder + file)
+    imageNos = list()
+    for file in imageFiles:
+        splitPath = os.path.splitext(file)
+        if splitPath[-1] == ".jpg":
+            imagePaths.append(datasetImagePaths + file)
+            imageNos.append(splitPath[-2])
     #imgTensor = torch.tensor([1, 3, test_width, test_height])
+
+    classNoDict={"ape": 0,
+                 "benchvise": 1,
+                 "can": 3,
+                 "cat": 4,
+                 "driller":5,
+                 "duck":6,
+                 "glue": 8}
+    colorDict={0:(255, 0, 0),
+               1:(0, 255, 0),
+               3:(0, 0, 255),
+               4:(255, 125, 0), 
+               5:(125, 125, 125),
+               6:(255, 125, 125),
+               8:(125, 255, 125)}
 
     for i in range(len(imagePaths)):
 
@@ -140,7 +173,7 @@ if __name__ == '__main__':
                 print(point)
         '''
 
-        img = cv2.imread(imageFilePaths[i])
+        img = cv2.imread(imagePaths[i])
         img_width = img.shape[1]
         img_height = img.shape[0]
         
@@ -166,9 +199,41 @@ if __name__ == '__main__':
         
         plot_boxes_cv2(img, candidateBoxes, '../Assets/Test/multi.jpg', 'nothing')
         '''
+        imageNo = imageNos[i]
 
+        labelFilePaths = list()
         for labelFolder in labelFolders:
-            labelFilePaths = os.listdir(labelFolders)
+            labelFilePaths.append(labelFolder + imageNo + ".txt")
+
+        labels = list()
+        #从中读取标签
+        for file in labelFilePaths:
+            if os.path.splitext(file)[-1] == ".txt":
+                labelfile = open(file, "r")
+                labelContent = labelfile.read()
+                label = labelContent.split(' ')
+                for i in range(21):
+                    label[i] = float(label[i])
+                labels.append(label)
+
+        #添加标签盒子
+        gt_boxes = list()
+        for label in labels:
+            box = label[1:19]
+            box.append(1)
+            box.append((int)(label[0]))
+            for pi in range(9):
+                box[2 * pi] = (int)(box[2 * pi] * img_width)
+                box[2 * pi + 1] = (int)(box[2 * pi + 1] * img_height)
+            gt_boxes.append(box)
+
+        classNames = ['ape', 'benchvise', 'can', 'cat', 'duck', 'driller', 'glue']
+        for bi in range(len(gt_boxes)):
+            gt_box = gt_boxes[bi]
+            if bi != (len(gt_boxes) - 1):
+                plot_boxes_cv2(img, gt_boxes[bi], None,  "fuck", colorDict[gt_box[19]])
+            else:
+                plot_boxes_cv2(img, gt_boxes[bi], outputPath + imageNo + ".jpg",  "fuck", colorDict[gt_box[19]])
 
 
             
